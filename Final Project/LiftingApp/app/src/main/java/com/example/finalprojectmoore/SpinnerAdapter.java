d1f1b6e94f404c3fad67a76aa4e45b792efff10b
GarMoore18 <62194724+GarMoore18@users.noreply.github.com>
2021-04-11 00:07:41 -0400

    Start percentage screen
    The base layout is finished. Needs tweaking and to fill in data. Will also need to have a volley request for this fragment.

---
+++ Final Project/LiftingApp/app/src/main/java/com/example/finalprojectmoore/SpinnerAdapter.java
@@ -0,0 +1,48 @@
+package com.example.finalprojectmoore;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SpinnerAdapter extends BaseAdapter {
+    Context context;
+    int exercise_imgs[];
+    String[] exercise_names;
+    LayoutInflater layoutInflater;
+
+    public SpinnerAdapter(Context applicationContext, int[] exercise_imgs, String[] exercise_names) {
+        this.context = applicationContext;
+        this.exercise_imgs = exercise_imgs;
+        this.exercise_names = exercise_names;
+        layoutInflater = (LayoutInflater.from(applicationContext));
+    }
+
+    @Override
+    public int getCount() {
+        return exercise_imgs.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view = layoutInflater.inflate(R.layout.custom_exercise_spinner, null);
+        ImageView icon = (ImageView) view.findViewById(R.id.imageView);
+        TextView names = (TextView) view.findViewById(R.id.textView);
+        icon.setImageResource(exercise_imgs[i]);
+        names.setText(exercise_names[i]);
+        return view;
+    }
+}
--- Final Project/LiftingApp/app/src/main/java/com/example/finalprojectmoore/ui/percentagecalculator/PercentageFragment.java
+++ Final Project/LiftingApp/app/src/main/java/com/example/finalprojectmoore/ui/percentagecalculator/PercentageFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,13 +16,20 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.finalprojectmoore.R;
+import com.example.finalprojectmoore.SpinnerAdapter;
 import com.example.finalprojectmoore.ui.platecalculator.PlatesViewModel;
 
 public class PercentageFragment extends Fragment {
 
+    // TODO: REQUEST WHEN CHANGE TO FRAGMENT --> BENCH, DEADLIFT, SQUAT MAX
+
     private PercentageViewModel percentageViewModel;
     private View root;
     private TextView textView;
+
+    private Spinner exercise_spin;
+    private String[] exercise_names = {"Bench", "Deadlift", "Squat"};
+    private int[] exercise_imgs = {R.drawable.spinner_bench, R.drawable.spinner_deadlift, R.drawable.spinner_squat};
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.percentage_fragment, container, false);
@@ -29,7 +39,16 @@ public class PercentageFragment extends Fragment {
 
         setPercentageViewModel();
 
+        // Find and set spinner
+        exercise_spin = root.findViewById(R.id.exercise_spinner);
+        spinnerSetup();
+
         return root;
+    }
+
+    private void spinnerSetup() {
+        SpinnerAdapter spinnerAdapter = new SpinnerAdapter(getActivity(), exercise_imgs, exercise_names);
+        exercise_spin.setAdapter(spinnerAdapter);
     }
 
     // Setup for viewmodel
